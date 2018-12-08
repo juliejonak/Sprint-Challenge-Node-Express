055b6702fcdaa6a0f148d5d311a3e025d0ef9af1
Julie Jonak <juliejonak10@gmail.com>
2018-12-08 11:38:07 -0600

    GET PROJECTS: -index.js: Added get() functionality for all projects

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const helmet = require('helmet');
 const logger = require('morgan');
-const server = express();
-
 const cors = require('cors');
+
+const actionDb = require('./data/helpers/actionModel');
+const projectDb = require('./data/helpers/projectModel');
+
+const server = express();
 
 server.use(
     express.json(),
@@ -14,6 +17,20 @@ server.use(
 
 const PORT = process.env.PORT || 4310;
 
+
+server.get('/projects', (req, res) => {
+    projectDb.get()
+        .then(projects => {
+            res.json(projects)
+        })
+        .catch(err => {
+            res
+            .status(500)
+            .json({
+                message: "Cound not fetch the projects."
+            })
+        })
+})
 
 
 server.listen(PORT, ()=>{
