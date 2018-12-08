3405722ed9903aa7cc395ecee1bf1f2871bca3b9
Julie Jonak <juliejonak10@gmail.com>
2018-12-08 13:27:06 -0600

    ADDED NAVIATION: -app.js: Adding nav links to the app

--- express/src/App.js
+++ express/src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import {Route, NavLink} from 'react-router-dom';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <nav>
+          <NavLink to='/'>Home</NavLink>
+          <NavLink to='/projects'>Projects</NavLink>
+          <NavLink to='/actions'>Actions</NavLink>
+          <NavLink to='/add-project'>Add New Project</NavLink>
+          <NavLink to='/add-action'>Add New Action</NavLink>
+        </nav>
+
+      
       </div>
     );
   }
